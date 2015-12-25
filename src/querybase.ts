6bcc8cdd50a0d002ad4c7f305aca035f02f0d340
David East <david@firebase.com>
2015-12-25 00:01:56 -0800

    Utils. Multiple sort by using ascii encoding.

--- src/querybase.ts
+++ src/querybase.ts
@@ -1,17 +1,73 @@
 /// <reference path="../typings/firebase/firebase.d.ts" />
 
-class QuerybaseQuery {
+class QuerybaseUtils {
   
-  constructor(query: FirebaseQuery) {
-    
+  codeify(key: string): string {
+    return key.split('').map((char) => { return char.charCodeAt(0) } ).join('');
+	}
+  
+  isString(value): boolean {
+    return typeof value === 'string' || value instanceof String;
+  }
+  
+  hasMultipleCriteria(criteriaKeys) {
+    return criteriaKeys.length > 1;
+  }
+  
+  createKey(propOne, propTwo) {
+    return `${propOne}_${propTwo}`;
+  }
+  
+  getPathFromRef(ref): string {
+    const PATH_POSITION = 3;
+    var pathArray = ref.toString().split('/');
+    return pathArray.slice(PATH_POSITION, pathArray.length).join('/');
+  }  
+  
+  merge(obj1, obj2) {
+    var mergedHash = {};
+    for (var prop in obj1) { 
+      mergedHash[prop] = obj1[prop]; 
+    }
+    for (var prop in obj2) { 
+      mergedHash[prop] = obj2[prop]; 
+    }
+    return mergedHash;
+  }
+  
+  values(obj) {
+    return Object.keys(obj).map(key => { return obj[key]; });
+  }
+  
+  arrayToObject(arr: any[]) {
+    var hash = {};
+    arr.forEach((item) => hash[item] = item);
+    return hash;
+  }
+  
+}
+
+class QuerybaseQuery {
+  query: FirebaseQuery;
+  
+  constructor(query) {
+    this.query = query;
   }
   
   lessThan(value) {
-    
+    return new QuerybaseQuery(this.query.endAt(value));
   }
   
   greaterThan(value) {
-    
+    return this.query.startAt(value);
+  }
+  
+  equalTo(value) {
+    return this.query.equalTo(value);
+  }
+  
+  on(event, callback, cancel?, context?) {
+    this.query.on(event, callback, cancel, context)
   }
   
 }
@@ -20,11 +76,13 @@ class Querybase {
   
   ref: Firebase;
   schema: any;
+  _: QuerybaseUtils;
   
   constructor(ref: Firebase, schema: any) {
     this.ref = ref;
     this.schema = schema;
-    var indexes = this._createIndexes(schema, this._arrayToObject(schema));
+    this._ = new QuerybaseUtils();
+    var indexes = this._createIndexes(schema, this._.arrayToObject(schema));
     this._warnAboutIndexOnRule(indexes);
   }
 
@@ -39,8 +97,8 @@ class Querybase {
   }
 
   push(data) {
-    var dataWithIndex = this._addIndexToData(this.schema, data);
-    this.ref.push().set(dataWithIndex);
+    const dataWithIndex = this._indexData(this.schema, data);
+    this.ref.parent().update(dataWithIndex);
   }
   
   remove() {
@@ -53,15 +111,15 @@ class Querybase {
   
   where(criteria): any {
     
-    if (this._isString(criteria)) {
+    if (this._.isString(criteria)) {
       return new QuerybaseQuery(this.ref.orderByChild(criteria));
     } 
     
     const keys = Object.keys(criteria);
-    const values = this._values(criteria);
+    const values = this._.values(criteria);
     
     // multiple criteria
-    if (this._hasMultipleCriteria(keys)) {
+    if (this._.hasMultipleCriteria(keys)) {
       var criteriaIndex = keys.join('_');
       var criteriaValues = values.join('_');
       return this.ref.orderByChild(criteriaIndex).equalTo(criteriaValues); 
@@ -69,18 +127,6 @@ class Querybase {
     
     // single criteria 
     return this.ref.orderByChild(keys[0]).equalTo(values[0]);
-  }
-  
-  private _isString(value): boolean {
-    return typeof value === 'string' || value instanceof String;
-  }
-  
-  private _hasMultipleCriteria(criteriaKeys) {
-    return criteriaKeys.length > 1;
-  }
-  
-  private _createKey(propOne, propTwo) {
-    return `${propOne}_${propTwo}`;
   }
   
   private _createIndexes(properties: any[], data: any, indexHash?: any) {
@@ -95,11 +141,11 @@ class Querybase {
       var propString = "";
       var valueString = "";
 
-      indexHash[this._createKey(mainProp, prop)] = this._createKey(data[mainProp], data[prop]);
+      indexHash[this._.createKey(mainProp, prop)] = this._.createKey(data[mainProp], data[prop]);
 
       propCop.forEach((subProp) => {
-        propString = this._createKey(propString, subProp);
-        valueString = this._createKey(valueString, data[subProp]);
+        propString = this._.createKey(propString, subProp);
+        valueString = this._.createKey(valueString, data[subProp]);
       });
       
       indexHash[mainProp + propString] = data[mainProp] + valueString;
@@ -113,43 +159,34 @@ class Querybase {
     return indexHash;
   }
   
-  private _getPathFromRef(ref): string {
-    const PATH_POSITION = 3;
-    var pathArray = ref.toString().split('/');
-    return pathArray.slice(PATH_POSITION, pathArray.length).join('/');
-  }  
-  
   private _addIndexToData(schema, data) {
     var indexes = this._createIndexes(schema, data);
-    var merged = this._merge(data, indexes);
+    var merged = this._.merge(data, indexes);
     return merged;
   }
   
-  private _merge(obj1, obj2) {
-    var mergedHash = {};
-    for (var prop in obj1) { 
-      mergedHash[prop] = obj1[prop]; 
-    }
-    for (var prop in obj2) { 
-      mergedHash[prop] = obj2[prop]; 
-    }
-    return mergedHash;
-  }
-  
-  private _values(obj) {
-    return Object.keys(obj).map(key => { return obj[key]; });
-  }
-  
-  private _arrayToObject(arr: any[]) {
-    var hash = {};
-    arr.forEach((item) => hash[item] = item);
-    return hash;
+  private _indexData(schema, data) {
+    var indexes = this._createIndexes(schema, data);
+    const merged = this._.merge(data, indexes);
+
+    const pushRef = this.ref.push();
+    const path = this._.getPathFromRef(this.ref);
+    
+    var indexHash = {};
+    Object.keys(indexes).forEach((index) => {
+      indexHash[`${path}_index/${index}/${this._.codeify(indexes[index])}`] = merged;
+    });
+    
+    var fanoutHash = {};
+    fanoutHash[`${path}/${pushRef.key()}`] = merged;
+    
+    return this._.merge(fanoutHash, indexHash);
   }
   
   private _warnAboutIndexOnRule(obj) {
-    var indexKeys = this._merge(obj, this._arrayToObject(this.schema));
+    var indexKeys = this._.merge(obj, this._.arrayToObject(this.schema));
     var indexOnRule =  `
-"${this._getPathFromRef(this.ref)}": {
+"${this._.getPathFromRef(this.ref)}": {
   ".indexOn": [${Object.keys(indexKeys).map((key) => { return `"${key}"`; }).join(", ")}]
 }`;
     console.warn(`Add this rule to improve performance of your Firebase queries: \n ${indexOnRule}`);
