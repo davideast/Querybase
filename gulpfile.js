fe86170eb0621123a771a1991c9398495a17dffc
David East <david@firebase.com>
2016-01-02 06:14:37 -0800

    chore(build): Istanbul for code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,65 @@ const del = require('del');
 const ts = require('gulp-typescript');
 const mocha = require('gulp-mocha');
 const runSequence = require('run-sequence');
+const istanbul = require('gulp-istanbul');
+const rename = require("gulp-rename");
+const merge = require('merge2');
 
-gulp.task('clean', () => del(['examples/*.js', 'examples/*.js.map', '!examples/index.js', 'dist']));
-
-gulp.task('typescript', ['clean'], () => {
-	return gulp.src('./src/Querybase.ts')
+const tsBuild = (config) => {  
+  config = config || {};
+  const path = config.path || './src/Querybase.ts';
+  const declaration = config.declaration || false
+  const tsResult = gulp.src(path)
     .pipe(sourcemaps.init())
 		.pipe(ts({
    		sortOutput: true,
 			module: 'commonjs',
 			target: 'es5',
-      out: 'querybase.js'
-	  }))
-    .pipe(sourcemaps.write())
-    .pipe(uglify())
+      out: 'querybase.js',
+      declaration: declaration
+	  }));
+    
+    if (declaration) {
+      return merge([
+		    tsResult.dts
+          .pipe(gulp.dest('./dist'))
+          .pipe(gulp.dest('./typings/querybase')),
+		    tsResult.js
+          .pipe(gulp.dest('./dist'))
+          .pipe(sourcemaps.write())
+	    ]); 
+    } 
+    
+    return tsResult.pipe(sourcemaps.write());
+};
+
+gulp.task('clean', () => del(['examples/*.js', 'examples/*.js.map', '!examples/index.js', 'dist']));
+
+gulp.task('typescript', ['clean'], () => {
+	return tsBuild({ declaration: true })
 		.pipe(gulp.dest('./dist'))
     .pipe(gulp.dest('./examples'));  
 });
 
-gulp.task('test', () => {
-  return gulp.src('./tests/unit/**.spec.js', { read: false })
-	 .pipe(mocha({ reporter: 'spec' }));
+gulp.task('min', () => {
+  return tsBuild()
+    .pipe(uglify())
+    .pipe(rename('quertbase.min.js'))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', () => runSequence('clean', 'typescript', 'test'));
+gulp.task('pre-test', function () {
+  return gulp.src(['./dist/querybase.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', () => {
+  return gulp.src('./tests/unit/**.spec.js', { read: false })
+	 .pipe(mocha({ reporter: 'spec' }))
+   .pipe(istanbul.writeReports());
+});
+
+gulp.task('default', () => runSequence('clean', 'typescript', 'min', 'pre-test', 'test'));
