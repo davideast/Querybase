2b511f98722c4d0490b7d050beee74622c2bd659
David East <david@firebase.com>
2016-01-03 07:14:51 -0800

    chore(tests): Querybase tests

--- tests/unit/querybase.spec.js
+++ tests/unit/querybase.spec.js
@@ -8,6 +8,7 @@ const firebaseServer = require('../firebaseServer');
 const QuerybaseQuery = Querybase.QuerybaseQuery;
 const assert = require('assert');
 const chai = require('chai');
+const sinon = require('sinon');
 const should = chai.should();
 const expect = chai.expect;
 
@@ -18,6 +19,29 @@ describe('Querybase', () => {
   const ref = new Firebase('ws://test.firebaseio.com:5000');
   
   it('should exist', () => { expect(Querybase).to.exist; });
+  
+  
+  describe('ref', () => {
+    it('should be a Firebase ref', () => {
+     const queryRef = new Querybase(ref, ['age', 'location']);
+     assert.equal(true, helpers.isFirebaseRef(queryRef.ref()));
+    });
+  });
+  
+  describe('set', () => {
+    
+    const queryRef = new Querybase(ref, ['age', 'location']);
+    
+    it("should call the Firebase set function", function () {
+        sinon.spy(queryRef.ref(), 'set');
+        
+        queryRef.set({ age: 27, location: 'SF' });
+        
+        expect(queryRef.ref().set.calledOnce).to.be.ok;
+        queryRef.ref().set.restore();
+    });
+    
+  });
   
   describe('where', () => {
     
@@ -31,6 +55,11 @@ describe('Querybase', () => {
     
     it('should return a Firebase query for an object with a single criteria', () => {
       const query = queryRef.where({ color: 'green' });
+      assert.equal(true, helpers.isFirebaseRef(query));
+    });
+    
+    it('should create a Firebase query for multiple criteria', () => {
+      const query = queryRef.where({ color: 'green', weight: '120' });
       assert.equal(true, helpers.isFirebaseRef(query));
     });
     
