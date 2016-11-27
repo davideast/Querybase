4be291ec2bd9a4797181afe6c0681a492eb05bc4
David East <deast@google.com>
2016-11-27 07:19:28 -0800

    chore(es6): Build system for UMD bundle

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,23 @@ const del = require('del');
 const mocha = require('gulp-mocha');
 const runSequence = require('run-sequence');
 const istanbul = require('gulp-istanbul');
-const connect = require('gulp-connect');
-const open = require('gulp-open');
 const firebaseServer = require('./tests/firebaseServer');
-const ts = require('gulp-typescript');
-const tsProject = ts.createProject('tsconfig.json');
 const uglify = require('gulp-uglify');
 const size = require('gulp-filesize');
- 
+const execSync = require('child_process').execSync;
+
 const exit = () => process.exit(0);
 
 gulp.task('clean', () => del(['examples/*.js', 'examples/*.js.map', '!examples/index.js', 'dist']));
 
-gulp.task('ts', function() {
-	const tsResult = tsProject.src().pipe(ts(tsProject));
-	return tsResult.js
-    .pipe(uglify())
-    .pipe(size())
-    .pipe(gulp.dest('./dist'))
-    .pipe(gulp.dest('./examples'));
+gulp.task('ts', () => {
+  const tsCode = execSync('tsc');
+  const rollupCode = execSync('node rollup.config');
 });
 
-gulp.task('pre-test', function () {
+gulp.task('pre-test', () => {
   return gulp
-    .src(['./dist/querybase.js'])
+    .src(['./dist/querybase.umd.js'])
     // Covering files
     .pipe(istanbul())
     // Force `require` to return covered files
@@ -36,7 +29,7 @@ gulp.task('pre-test', function () {
 });
 
 // Use for build process, continues stream
-gulp.task('test', ['firebaseServer', 'typings', 'pre-test'], () => {
+gulp.task('test', ['firebaseServer', 'pre-test'], () => {
   return gulp
    .src('./tests/unit/**.spec.js', { read: false })
 	 .pipe(mocha({ reporter: 'spec' }))
@@ -47,29 +40,4 @@ gulp.task('firebaseServer', () => {
   firebaseServer.initializeApp();
 });
 
-gulp.task('coverageServer', () => {
-  connect.server({
-    root: 'coverage',
-    port: 8001,
-    livereload: true
-  });
-});
-
-gulp.task('html', function () {
-  gulp.src('./coverage/*.html')
-    .pipe(connect.reload());
-});
- 
-gulp.task('watch', function () {
-  gulp.watch(['./coverage/*.html'], ['html']);
-});
-
-gulp.task('coverage', ['watch', 'coverageServer']);
-
-gulp.task('typings', () => {
-  return gulp
-    .src('./tests/**/*.d.ts')
-    .pipe(gulp.dest('./typings'));
-});
-
-//gulp.task('default', runSequence('clean', 'ts', 'test', exit));
+gulp.task('default', runSequence('clean', 'ts', 'test', exit));
